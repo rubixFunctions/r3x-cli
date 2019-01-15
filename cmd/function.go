b836ffa1044bbcf3fe53d2a400f3dfe1ea2295f0
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-01-15 10:49:04 +0000

    add initial init cmd, and function strut

---
+++ cmd/function.go
@@ -0,0 +1,20 @@
+package cmd
+
+type Function struct {
+	absPath string
+	cmdPath string
+	srcPath string
+	//Add License type
+	//license License
+	name string
+}
+
+// Returns Function with a specified function name
+func NewFunction(functionName string) *Function {
+
+}
+
+// Returns Function with a specified absolute path to package
+func NewFunctionFromPath(absPath string) *Function {
+
+}
--- cmd/init.go
+++ cmd/init.go
@@ -16,35 +16,44 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:     "init [name]",
+	Aliases: []string{"initialize", "initialise", "create"},
+	Short:   "Initialize a Function as a Container",
+	Long: `Initialize (r3x init) will create a new Function as a container, 
+with a license and the appropriate structure needed for a Knative Function.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	- If an absolute path is provided, it will be created.
+	
+Init will not use an existing directory with contents.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Print(err)
+		}
+
+		var function *Function
+		if len(args) == 0 {
+			function = NewFunctionFromPath(wd)
+		}
+
+		initializeFunction(function)
+		fmt.Println("init called ", wd)
+
 	},
+}
+
+func initializeFunction(function *Function) {
+
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
